0d9994550c2f89a370e383b0e8ab8017dd6138df
James Palovich <palovich003@gmail.com>
2023-04-27 20:40:02 -0400

    goofy numbers

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+import random
+
 print('this is only a test')
 print('this is a proof of concept for myself')
 print('thank you for silently judging me :)')
 print('Additional test')
 print('test commit')
 print('change')
+print(0.1 + 0.1 + 0.1)
+
+for i in range(10):
+    i = random.random()
+    print(i + 1) 
